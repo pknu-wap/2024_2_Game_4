f43f5e710e2578fa83ca3a20b54ea0408cc2aef9
hhchan15 <huhchan515@pukyong.ac.kr>
2024-11-14 10:21:21 +0900

    시간 제한 기능 구현
    시간 제한 기능과 남은 시간을 표시하는 타이머 구현
    LobbyManager에 타이머를 실행하는 코드 추가

--- Assets/02. Scripts/Lobby/LobbyManager.cs
+++ Assets/02. Scripts/Lobby/LobbyManager.cs
@@ -92,6 +92,7 @@ public class LobbyManager : MonoBehaviour
         singlePlayer.SetActive(true);
         SwapScene();
         CountManager.instance.GameStart();
+        TimeLimiter.instance.StartTimer();
     }
     
     //로비 종료 함수
