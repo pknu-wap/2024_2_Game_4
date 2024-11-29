b6506b1196d0bc1995dce12ad730bbf8ed0a1d2f
차동근 <127104912+Kkumteulyi@users.noreply.github.com>
2024-11-29 15:30:20 +0900

    변경 사항 업로드

--- Assets/02. Scripts/Lobby/LobbyManager.cs
+++ Assets/02. Scripts/Lobby/LobbyManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class LobbyManager : MonoBehaviour
 {
@@ -10,12 +11,6 @@ public class LobbyManager : MonoBehaviour
     
     [Header("멀티 플레이 팝업창")]
     public GameObject multiPannel;
-
-    [Header("메뉴 팝업창")]
-    public GameObject menuPannel;
-    
-    [Header("랭킹 팝업창")]
-    public GameObject rankPannel;
     
     [Header("게임 씬")]
     public GameObject gameScene;
@@ -25,10 +20,13 @@ public class LobbyManager : MonoBehaviour
 
     [Header("컨트롤 패드")] 
     public GameObject controlPad;
-
+    
     [Header("싱글 플레이어")] 
     public GameObject singlePlayer;
+
+    public GameObject selectedMap;
     
+    public GameObject reloadButton;
     // 다른 스크립트에서 사용하기 위해 싱글톤 생성
     public static LobbyManager instance;
     
@@ -48,8 +46,6 @@ public class LobbyManager : MonoBehaviour
         // 팝업창 비활성화
         singlePannel.SetActive(false);
         multiPannel.SetActive(false);
-        menuPannel.SetActive(false);
-        rankPannel.SetActive(false);
         // 게임 씬 비활성화
         gameScene.SetActive(false);
         // 컨트롤 패드 비활성화
@@ -68,16 +64,6 @@ public class LobbyManager : MonoBehaviour
     {
         multiPannel.SetActive(!multiPannel.activeSelf);
     }
-    //랭킹 패널 팝업 창
-    public void ShowRankPannel()
-    {
-        rankPannel.SetActive(!rankPannel.activeSelf);
-    }
-    //메뉴 패널 팝업 창
-    public void ShowMenuPannel()
-    {
-        menuPannel.SetActive(!menuPannel.activeSelf);
-    }
     // 게임 씬과 로비 씬 전환 함수
     public void SwapScene()
     {
@@ -85,10 +71,26 @@ public class LobbyManager : MonoBehaviour
         gameScene.SetActive(!gameScene.activeSelf);
         lobbyScene.SetActive(!gameScene.activeSelf);
     }
+
+    public void SetMap(GameObject map)
+    {
+        selectedMap = map;
+    }
+
+    public void ShowReloadButton()
+    {
+        reloadButton.SetActive(true);
+    }
+    
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
     
     //싱글 플레이 시작 함수
     public void StartSingleGame()
     {   
+        selectedMap.SetActive(true);
         singlePlayer.SetActive(true);
         SwapScene();
         CountManager.instance.GameStart();
